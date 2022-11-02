283c6f6b4fbe42bfb5b9931a148694ec2e5761d7
Sami Kuikka <sami.kuikka@aalto.fi>
2022-11-02 20:30:57 +0200

    /api/workouts POST

--- __tests__/workouts.test.js
+++ __tests__/workouts.test.js
@@ -9,13 +9,10 @@ const WorkoutTemplate = require('../models/WorkoutTemplate');
 const ExerciseName = require('../models/ExerciseName');
 
 
-let workout_id = null;
-let token = null;
 // reset database for tests
 beforeEach( async () => {
     await User.deleteMany({});
     await Exercise.deleteMany({});
-    await WorkoutTemplate.deleteMany({});
     await ExerciseName.deleteMany({});
     //Add 4 exercise name for tests
     await ExerciseName.create(
@@ -25,26 +22,33 @@ beforeEach( async () => {
         {_id: 3, exercise: "Deadlift"}
     );
     
-    //Add new workout template
-    const new_w = new WorkoutTemplate({
-        id: 0,
-        name: "Full body",
-        exercises: [0,1,3]
-    });
-    const saved = await new_w.save();
-    workout_id = saved._id;
-    
-    //Log in before test
-    const passwordHash = await bcrypt.hash('sekret', 10);
-    const user = new User({username: 'test', passwordHash, workouts: [workout_id]});
-    await user.save();
-    let res = await api
-        .post('/api/login')
-        .send({username: 'test', password: 'sekret'});
-    token = res.body.token;
 });
 
 describe('GET', () => {
+
+    let workout_id = null;
+    let token = null;
+
+    beforeEach( async () => {
+        await WorkoutTemplate.deleteMany({});
+        //Add new workout template
+        const new_w = new WorkoutTemplate({
+            id: 0,
+            name: "Full body",
+            exercises: [0,1,3]
+        });
+        const saved = await new_w.save();
+        workout_id = saved._id;
+        
+        //Log in before test
+        const passwordHash = await bcrypt.hash('sekret', 10);
+        const user = new User({username: 'test', passwordHash, workouts: [workout_id]});
+        await user.save();
+        let res = await api
+            .post('/api/login')
+            .send({username: 'test', password: 'sekret'});
+        token = res.body.token;
+    });
 
     test('User should have 1 workout', async () => {
         const user = await User.findOne({username: 'test'}).populate('workouts');
@@ -65,3 +69,73 @@ describe('GET', () => {
     });
 
 })
+
+describe('POST', () => {
+
+    let workout_id = null;
+    let token = null;
+
+    beforeEach( async () => {
+        await WorkoutTemplate.deleteMany({});
+        //Add new workout template
+        const new_w = new WorkoutTemplate({
+            id: 0,
+            name: "Full body",
+            exercises: [0,1,3]
+        });
+        const saved = await new_w.save();
+        workout_id = saved._id;
+        
+        //Log in before test
+        const passwordHash = await bcrypt.hash('sekret', 10);
+        const user = new User({username: 'test', passwordHash, workouts: [workout_id]});
+        await user.save();
+        let res = await api
+            .post('/api/login')
+            .send({username: 'test', password: 'sekret'});
+        token = res.body.token;
+    })
+
+    test('User should have 1 workout', async () => {
+        const user = await User.findOne({username: 'test'}).populate('workouts');
+        
+        expect(user.workouts).toHaveLength(1);
+        expect(user.workouts[0].name).toBe('Full body');
+    });
+
+    test('request without id or name gets rejected', async () => {
+        const response = await api
+            .post('/api/workouts')
+            .set('Authorization', `bearer ${token}`)
+            .send({id: 2})
+            .expect(400);
+        expect(response.text).toBe('id and name required')
+    });
+
+    test('no dublicate ids accepted', async () => {
+        const response = await api
+            .post('/api/workouts')
+            .send({id: 0, name: 'Test'})
+            .set('Authorization', `bearer ${token}`)
+            .expect(400);
+        expect(response.text).toBe('duplicate id')
+    });
+
+    test('can post valid template', async () => {
+        const response = await api
+            .post('/api/workouts')
+            .send({id: 1, name: 'Leg workout', exercises: [1,2,4]})
+            .set('Authorization', `bearer ${token}`)
+            .expect(201);
+
+        // now user should have two workouts
+        const user = await User.findOne({username: 'test'}).populate('workouts');
+        expect(user.workouts).toHaveLength(2);
+        expect(response.body.workouts).toHaveLength(2);
+        
+        //and newly added workout should be accessible from the user
+        expect(user.workouts.map(e => e.name)).toEqual(expect.arrayContaining(['Leg workout']));
+        expect(user.workouts.map(e => e.exercises)).toEqual(expect.arrayContaining([[1,2,4]]))
+    });
+
+});
--- routes/workouts.js
+++ routes/workouts.js
@@ -15,6 +15,34 @@ workoutsRouter.get('/', userExtractor, async (request, response) => {
     response.status(200).json(obj.workouts);
 });
 
+// Add new template to the users workouts
+workoutsRouter.post('/', userExtractor, async (request, response) => {
+    const body = request.body;
+    const user = request.user;
+    
+    // Request should at least have id and name
+    if(body.id == null || body.name == null) {
+        return response.status(400).send('id and name required');
+    }
 
+    //Check that no dublicate id
+    const user_model = await User.findById(user._id).populate('workouts');
+    if(user_model.workouts.some(workout => workout.id === body.id) ) {
+        return response.status(400).send('duplicate id');
+    }
+
+    // Create new template to the db
+    const template = new WorkoutTemplate({
+        ...body
+    });
+    const saved = await template.save();
+
+    // Update the user workout list
+    let current_user = await User.findById(user._id);
+    current_user.workouts = [...current_user.workouts, saved._id];
+    const res = await current_user.save();
+
+    response.status(201).json(res);
+});
 
 module.exports = workoutsRouter;
